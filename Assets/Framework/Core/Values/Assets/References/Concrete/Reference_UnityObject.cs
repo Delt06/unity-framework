6af015deedd8bca6a7554d4bf63609ad719af1e1
Vladislav Kantaev <vvkantaev@yandex.ru>
2019-12-10 15:25:47 +0300

    Added unity object var+ref

---
+++ Assets/Framework/Core/Values/Assets/References/Concrete/Reference_UnityObject.cs
@@ -0,0 +1,11 @@
+using System;
+using Core.Values.Assets.References;
+using Framework.Core.Values.Assets.Variables.Concrete;
+
+namespace Framework.Core.Values.Assets.References.Concrete
+{
+    [Serializable]
+    public class Reference_UnityObject : Reference<UnityEngine.Object, Variable_UnityObject>
+    {
+    }
+}
---
+++ Assets/Framework/Core/Values/Assets/Variables/Concrete/Variable_UnityObject.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Framework.Core.Values.Assets.Variables.Concrete
+{
+    [CreateAssetMenu(menuName = BuiltInPath + "Unity Object")]
+    public class Variable_UnityObject : Variable<Object>
+    {
+        
+    }
+}
