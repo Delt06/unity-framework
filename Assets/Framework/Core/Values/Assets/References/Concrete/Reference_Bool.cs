2a91ca75eb16075cac3197c589549b5cf5c2825f
Vladislav Kantaev <vvkantaev@yandex.ru>
2019-12-14 22:20:46 +0300

    Added boolean variable/reference pair

---
+++ Assets/Framework/Core/Values/Assets/References/Concrete/Reference_Bool.cs
@@ -0,0 +1,15 @@
+using System;
+using Core.Values.Assets.References;
+using Framework.Core.Values.Assets.Variables.Concrete;
+
+namespace Framework.Core.Values.Assets.References.Concrete
+{
+    [Serializable]
+    public class Reference_Bool : Reference<bool, Variable_Bool>
+    {
+        public static implicit operator Reference_Bool(bool value)
+        {
+            return new Reference_Bool {Constant = value};
+        }
+    }
+}
---
+++ Assets/Framework/Core/Values/Assets/Variables/Concrete/Variable_Bool.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Framework.Core.Values.Assets.Variables.Concrete
+{
+    [CreateAssetMenu(menuName = BuiltInPath + "Boolean")]
+    public class Variable_Bool : Variable<bool>
+    {
+        
+    }
+}
